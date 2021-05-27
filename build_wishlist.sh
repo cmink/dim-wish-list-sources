8f93dcc7ae7051f8c1d55de18205bf384e8d7fd9
Craig Mink <cwmink@pacbell.net>
2021-05-27 11:41:33 -0700

    Testing out a build_wishlist script
    - Thanks Veridas1000!

---
+++ build_wishlist.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+# Shamelessly copying Veritas1000's wishlist builder script
+# https://github.com/Veritas1000/dim-wish-list-sources
+
+BUILT_WISHLIST_FILE="test_choosy_craigmink.txt"
+BUILT_WISHLIST_TITLE="A compiled wishlist generated from sublists"
+
+MKB=false
+CONTROLLER=true
+INCLUDE_TRASH=true
+
+add_sublist() {
+    echo "Adding $1"
+
+    echo "// source file: $1" >> $BUILT_WISHLIST_FILE
+    cat $1 >> $BUILT_WISHLIST_FILE
+    echo "" >> $BUILT_WISHLIST_FILE
+}
+
+echo "Combining sublists into ${BUILT_WISHLIST_FILE}:"
+
+echo "title: ${BUILT_WISHLIST_TITLE}" > $BUILT_WISHLIST_FILE
+echo "" >> $BUILT_WISHLIST_FILE
+
+
+#Panda's lists
+for sublist in PandaPaxxy/*.txt;do
+    if [[ $sublist == *"controller"* && "$CONTROLLER" = false ]] \
+        || [[ $sublist == *"mkb"* && "$MKB" = false ]]
+    then
+        echo "-- SKIPPING $sublist"
+    else
+        add_sublist $sublist
+    fi
+done
+
+# Mercules904's lists
+for sublist in Mercules904/*.txt;do
+    add_sublist $sublist
+done
+
+# AyyItsChevy's lists
+for sublist in AyyItsChevy/*.txt;do
+    add_sublist $sublist
+done
+
+# craigmink's lists
+for sublist in `ls craigmink/*.txt | sort -Vr`;do
+    add_sublist "$sublist"
+done
+
+if [ "$INCLUDE_TRASH" = true ];then
+    for sublist in "$(ls -1 */*trash*.txt | sort -r)";do
+        add_sublist "$sublist"
+    done
+fi
